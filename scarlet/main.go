6ab9cb1f5018d46624a268bb55cd739d7055e16b
Nick Saika <nicksaika@gmail.com>
2012-10-02 22:55:05 -0400

    Look at your code, now back to me, now back to your code. What do you see? Your code now has a mainloop.

--- scarlet/main.go
+++ scarlet/main.go
@@ -4,7 +4,37 @@
 //
 package main
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	systemSignals = make(chan os.Signal)
+)
+
 func main() {
-	println("do something...")
+	println("starting scarlet")
+	startSignalListener()
 	return
+}
+
+func startSignalListener() {
+	signal.Notify(systemSignals, syscall.SIGINT, syscall.SIGKILL, syscall.SIGHUP)
+	for {
+		sig := <-systemSignals
+		switch sig {
+		case syscall.SIGINT:
+			println("caught SIGINT; exiting")
+			os.Exit(0)
+		case syscall.SIGKILL:
+			println("caught SIGKILL; exiting")
+			os.Exit(0)
+		case syscall.SIGHUP:
+			println("caught SIHUP; reloading...")
+			// call a function to reload the configuration, and
+			// re-initialize any connections (if necessary)
+		}
+	}
 }
