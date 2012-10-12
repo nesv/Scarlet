6eaa177e6bcf3c3587746adcf021e43d4fe0a1a7
Nick Saika <nicksaika@gmail.com>
2012-10-11 21:40:55 -0400

    Added some much-needed comments to the redis.go functions.

--- redis.go
+++ redis.go
@@ -15,6 +15,10 @@ var (
 	InfoDbRegex = regexp.MustCompile(`db(\d{1,3})`)
 )
 
+// A ConnectionMap holds a collection of Redis clients, and provides a nice,
+// easy way to quickly establish a new connection to a database on a Redis
+// host, and get the appropriate connection for the incoming HTTP request.
+//
 type ConnectionMap struct {
 	netaddr     string
 	password    string
@@ -22,16 +26,24 @@ type ConnectionMap struct {
 	connections map[int]*redis.Client
 }
 
+// Creates (and returns) a pointer to a ConnectionMap.
+//
 func NewConnectionMap(netaddr, password string) (cm *ConnectionMap) {
 	cm = &ConnectionMap{netaddr: netaddr, password: password}
 	return
 }
 
+// Associates a Redis client to the database it is connected to, so it can be
+// kept around for future use.
+//
 func (c *ConnectionMap) Add(db int, rc *redis.Client) {
 	c.connections[db] = rc
 	return
 }
 
+// Populates connections to any database. on the Redis host the ConnectionMap
+// was initialized with, that holds data.
+//
 func (cm *ConnectionMap) PopulateConnections() (err error) {
 	client := redis.New(cm.netaddr, 0, cm.password)
 	info, e := GetHostInfo(client)
@@ -59,6 +71,9 @@ func (cm *ConnectionMap) PopulateConnections() (err error) {
 	return
 }
 
+// Runs the INFO command on the remote Redis host, and nicely maps the response
+// from the server to a string-string map.
+//
 func GetHostInfo(c *redis.Client) (info map[string]string, err error) {
 	elem, err := c.Info()
 	if err != nil {
